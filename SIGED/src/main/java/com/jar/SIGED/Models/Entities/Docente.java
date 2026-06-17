e4aa7ee3360a08710c90c4e052c14421816338a8
adrielDuoc <adr.yanez@duocuc.cl>
2026-06-17 13:59:58 -0400

    Creacion de Alumno y Docente

--- SIGED/src/main/java/com/jar/SIGED/Models/Entities/Docente.java
+++ SIGED/src/main/java/com/jar/SIGED/Models/Entities/Docente.java
@@ -6,32 +6,30 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
-@AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "Alumnos")
-public class Alumno {
+@AllArgsConstructor
+@Table(name = "Docentes")
+public class Docente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer id_Docente;
 
     @NotBlank
-    private String nombre_Alumno;
+    private String nombre_Docente;
 
     @NotBlank
-    private String apellido_Alumno;
-
-    @NotNull
-    private Integer edad_Alumno;
+    private String apellido_Docente;
 
     @NotBlank
-    private String rut_Alumno;
+    private String rut_Docente;
 
+    @NotBlank
+    private String asignatura_Docente;
 }
