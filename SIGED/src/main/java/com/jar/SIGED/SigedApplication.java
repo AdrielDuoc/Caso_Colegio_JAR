ed106914219e83c310e60c0670484a2e4bfffaed
Adriel Yañez <adr.yanez@duocuc.cl>
2026-06-17 13:30:31 -0400

    Se creo el proyecto

---
+++ SIGED/src/main/java/com/jar/SIGED/SigedApplication.java
@@ -0,0 +1,13 @@
+package com.jar.SIGED;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigedApplication.class, args);
+	}
+
+}
---
+++ SIGED/src/test/java/com/jar/SIGED/SigedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jar.SIGED;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
