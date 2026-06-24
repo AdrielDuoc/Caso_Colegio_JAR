9952567b729dbf90c6617f3e9fc6266abf7d0448
vicentevergara1 <vice.vergaras@duocuc.cl>
2026-06-24 13:00:42 -0400

    Actualizar Asignatura.java

--- SIGED/src/main/java/com/jar/SIGED/Models/Entities/Asignatura.java
+++ SIGED/src/main/java/com/jar/SIGED/Models/Entities/Asignatura.java
@@ -1,12 +1,7 @@
+
 package com.jar.SIGED.Models.Entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -25,7 +20,12 @@ public class Asignatura {
     private Integer idAsignatura;
 
     @NotBlank
-    private String nombreAsignatura;
+    private String nombre;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "idCurso")
+    private Curso curso;
 
     @NotNull
     @ManyToOne
